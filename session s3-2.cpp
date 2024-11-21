67796d9cac739ea06db214ab4dc944f88a451fc2
Bichngocc07 <ngoc01072006@gmail.com>
2024-11-22 00:41:41 +0700

    sessions3-s2

---
+++ session s3-2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	float celsius, fahrenheit;
+	
+	printf("Nhap nhiet do theo do Celsius: ");
+	scanf("%f", &celsius);
+	
+	float c,f;
+	scanf("%f",&c);
+	
+	printf("Nhiet do theo do Fahrenheit: %.2f\n");
+
+
+}
+
+
+
+
