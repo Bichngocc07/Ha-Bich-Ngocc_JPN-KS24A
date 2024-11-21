7704345cba310466e65320ad10399aa4dac1070e
Bichngocc07 <ngoc01072006@gmail.com>
2024-11-21 13:26:15 +0700

    ss02-s4

--- Session s2-04.cpp
+++ Session s2-04.cpp
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 int main(){
-	// Khai bao va khoi tao hai gia tri ban dau
-	int a = 20; // Gia tri dau tien
-	int b = 5; // Gia tri thu hai
 	
-	// Khai bao them cac bien de luu ket qua 
-	int hieu = a - b; // Hieu cua a va b
-	int tich = a * b; // Tich cua a va b
-	float thuong = (float)a / b; // Thuong cua a va b (ep kieu de co so thuc)
+	int a = 20;
+	int b = 5;
 	
-	// In ra ket qua 
+	
+	int hieu = a - b;
+	int tich = a * b;
+	float thuong = (float)a / b;
+	
+	
 	printf("Gia tri ban dau: a = %d, b = %d\n", a, b);
 	printf("Hieu (a - b) = %d\n", hieu);
 	printf("Tich (a * b) = %d\n", tich);
