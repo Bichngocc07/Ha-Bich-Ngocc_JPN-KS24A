5608278779fe18841e85826a696ba41ee7bd8a99
Bichngocc07 <ngoc01072006@gmail.com>
2024-11-22 05:52:07 +0700

    session03-s7

---
+++ sessions3-07.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int num, sum = 0;
+    
+    printf("Nhap mot so nguyen co 4 chu so: ");
+    scanf("%d", &num)
+    
+     printf("Vui long nhap mot so nguyen co 4 chu so.\n")
+
+    printf("Tong cac chu so: %d\n", sum);
+
+    return 0;
+}
+
