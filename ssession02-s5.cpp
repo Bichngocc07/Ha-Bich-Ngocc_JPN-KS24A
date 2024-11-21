60111efd27f6e3fca6d335f9476a6c79b3d22d36
Bichngocc07 <ngoc01072006@gmail.com>
2024-11-21 08:54:50 +0700

    ssion02-s5

---
+++ ssession02-s5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    double chieuDai = 10.0; 
+    double chieuRong = 5.0; 
+
+    
+    double chuVi = 2 * (chieuDai + chieuRong);
+    double dienTich = chieuDai * chieuRong;
+
+    
+    cout << "Chieu dai: " << chieuDai << endl;
+    cout << "Chieu rong: " << chieuRong << endl;
+    cout << "Chu vi: " << chuVi << endl;
+    cout << "Dien tích: " << dienTich << endl;
+
+    return 0;
+}
+
