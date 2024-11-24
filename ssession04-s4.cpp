5d82532b3d2fce975ec31b33ff7b131e74ecb10b
Bichngocc07 <ngoc01072006@gmail.com>
2024-11-24 21:50:40 +0700

    session04-s4

---
+++ ssession04-s4.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+    printf("Moi ban nhap so thang:");
+    scanf("%d", &month);
+    
+    switch (month){
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+	printf("Thang %d co 31 ngay", month);
+    break;
+    
+    case 4: case 6: case 9: case 11:
+    printf("Thang %d co 30 ngay", month);
+    break;
+    
+    case 2:
+    printf("Thang %d co 28 hoac 28 ngay", month);
+    break;
+    
+    default:
+    printf("Thang khong hop le");	
+	}
+    return 0;
+}
+
