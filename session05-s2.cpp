cd1adc3b35fd6ac4a02c9c4811bcc0ea6daa77f3
Bichngocc07 <ngoc01072006@gmail.com>
2024-11-25 21:45:33 +0700

    session05-s2

---
+++ session05-s2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int number = 54;
+    int i;
+    do{
+    	printf("Nhap vao mot so nguyen: ");
+        scanf("%d", &i);
+	}  while (i != 54);
+	printf("Chuc mung ban da nhap dung: ");
+
+    return 0 ;
+}
+
+
