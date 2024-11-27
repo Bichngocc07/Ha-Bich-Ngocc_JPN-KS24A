0590c75f578de28ee3b48dcc1aa3837936a9eff3
Bichngocc07 <ngoc01072006@gmail.com>
2024-11-27 07:39:37 +0700

    session06-s2

---
+++ session06-s2.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[5];
+    int evenCount = 0, oddCount = 0;
+    
+
+    printf("Nhap vao 5 so nguyen: ");
+    for (int i = 0; i < 5; i++) {
+        numbers[i];
+    }
+
+    
+    for (int i = 0; i < 5; i++) {
+        if (numbers[i] % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+
+    
+    printf("So luong so chan: ");
+    printf("So luong so le: ");
+
+    return 0;
+}
+
+
+
+
