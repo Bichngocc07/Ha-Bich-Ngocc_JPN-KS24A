4cd379a582fdeea46efbe607c331ff27b5694f1d
Bichngocc07 <ngoc01072006@gmail.com>
2024-11-28 00:55:51 +0700

    session07-s4

---
+++ session07-s4.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Nhap vao so phan tu cua mang: ");
+    scanf("%d", &n);
+    int array[n];
+
+    
+    printf("%d", &array[i]);
+    for (int i = 0; i < n; i++) {
+        
+    }
+
+    
+    printf("Cac phan tu trong mang la: ");
+    for (int i = 0; i < n; i++) {
+    	printf("%d", &array);
+        
+    }
+    
+
+    
+
+    return 0;
+}
+
