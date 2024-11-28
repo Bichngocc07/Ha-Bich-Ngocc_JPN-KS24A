6fe97b0d188624913ad80d4da6502e4b76befd45
Bichngocc07 <ngoc01072006@gmail.com>
2024-11-28 13:20:17 +0700

    Update session07-s2.cpp

--- session07-s2.cpp
+++ session07-s2.cpp
@@ -1,21 +1,15 @@
-#include <iostream>
-
-int main() {
-    int array[5];
-
-    
-    printf("Nhap vao 5 so nguyen: ");
-    for (int i = 0; i < 5; i++) {
-        scanf("%d", &array[i]);
+#include <stdio.h>
+int main(){   
+    int arr[5];
+    printf("Moi ban nhap vao 5 phan tu cho mang:\n");
+    for (int i = 0; i < 5; i++){
+        printf("Hay nhap phan tu thu %d: ", i + 1);
+        scanf("%d", &arr[i]);
     }
-
-    
-    printf("Cac phan tu trong mang la: ");
-    for (int i = 0; i < 5; i++) {
-        printf("phan tu thu %d : %d\n",i+1,array[i]);
+    printf("\n Mang ma ban vua nhap la:\n");
+    for (int i = 0; i < 5; i++){
+        printf("%d ", arr[i]);
     }
-    
-
     return 0;
 }
 
