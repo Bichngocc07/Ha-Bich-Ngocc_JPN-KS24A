eda828aa88136fa9413b4ef3d8468bfbd31ac524
Bichngocc07 <ngoc01072006@gmail.com>
2024-12-10 16:35:36 +0700

    Add files via upload

--- B24DTCN0410.cpp
+++ B24DTCN0410.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-// Hàm tính Entropy
 double calculateEntropy(double p) {
     if (p == 0 || p == 1) {
-        return 0.0; // Entropy b?ng 0 khi p = 0 ho?c p = 1
+        return 0.0;
     }
     return -p * log2(p) - (1 - p) * log2(1 - p);
 }
@@ -22,8 +21,7 @@ int main() {
         printf("Xac suat khong hop le! Vui long nhap gia tri trong khoang [0, 1].\n");
         return 1;
     }
-
-    // Tính Entropy và hi?n th? k?t qu?
+    
     double entropy = calculateEntropy(p);
     printf("Entropy H(X) = %lf\n", entropy);
 
