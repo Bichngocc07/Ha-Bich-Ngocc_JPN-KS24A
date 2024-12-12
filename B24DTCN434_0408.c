d9a7f5440875988d6c1820cfa10003ecb5816925
Bichngocc07 <ngoc01072006@gmail.com>
2024-12-12 19:11:15 +0700

    Add files via upload

---
+++ B24DTCN434_0408.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+const double EPSILON_0 = 8.854e-12; 
+
+double tinhLucF(double b, double K, double V, double d) {
+    return (EPSILON_0 * b * (K - 1) * V * V) / (2 * d);
+}
+
+int main() {
+    double b, K, V, d;
+
+    printf("Nhap gia tri b (m): ");
+    if (scanf("%lf", &b) != 1 || b <= 0) {
+        printf("Gia tri b khong hop le.\n");
+        return 1;
+    }
+
+    printf("Nhap gia tri K (he so): ");
+    if (scanf("%lf", &K) != 1 || K <= 1) {
+        printf("Gia tri K phai lon hon 1.\n");
+        return 1;
+    }
+
+    printf("Nhap gia tri V (V): ");
+    if (scanf("%lf", &V) != 1 || V <= 0) {
+        printf("Gia tri V khong hop le.\n");
+        return 1;
+    }
+
+    printf("Nhap gia tri d (m): ");
+    if (scanf("%lf", &d) != 1 || d <= 0) {
+        printf("Gia tri d khong hop le.\n");
+        return 1;
+    }
+
+    double F = tinhLucF(b, K, V, d);
+    printf("Luc F la: %lf\n", F);
+
+    return 0;
+}
+
