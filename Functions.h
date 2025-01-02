2ed56ad8209ab30e6619155ac2240bf04f694aa5
Bichngocc07 <ngoc01072006@gmail.com>
2025-01-02 20:28:13 +0700

    Add files via upload

---
+++ Functions.h
@@ -0,0 +1,17 @@
+#include "Datatypes.h"
+//Nguyen mau ham
+void displayStartMenu();
+int displayLogin();
+void displayAdminMenu();
+void displayUserName();
+void addUser();
+void show_users();
+void show_user_details(int index);
+//void lock_unlock_user();
+void lockUnlockUser(); 
+void displayUserID();
+void sortedUsers();
+void searchUserByName();
+void displayUserList();
+void saveUsersToFile(const char* filename);
+void loadUsersFromFile(const char* filename);
